c87fc06104f1c3f30b2706ace3c4f1df6191e152
Brecht Baekelandt <brecht.baekelandt@outlook.com>
2018-02-09 19:30:40 +0100

    Ftp backup task for VSTS.

---
+++ ftp-backup.cmd
@@ -0,0 +1,9 @@
+echo off
+set username=%1
+set pasword=%2
+set root=%3
+shift
+shift
+shift
+wget -m --ftp-user=%username% --ftp-password=%password% ftp://ftp-eu.site4now.net/brechtbaekelandt/wwwroot/images/blog/ -P %root%/wwwroot/images/blog/
+wget -m --ftp-user=%username% --ftp-password=%password% ftp://ftp-eu.site4now.net/brechtbaekelandt/wwwroot/attachments/blog/ -P %root%/wwwroot/attachments/blog/
