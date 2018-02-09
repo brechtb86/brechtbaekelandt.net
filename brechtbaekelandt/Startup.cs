b400dd9b616ae0b317ed8168ebdca6b28ad08d79
Brecht Baekelandt <brecht.baekelandt@outlook.com>
2018-02-09 11:40:34 +0100

    Using HTTPS on production server.

--- brechtbaekelandt/Startup.cs
+++ brechtbaekelandt/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -195,13 +196,13 @@ namespace brechtbaekelandt
 
             app.UseSession();
 
-            //if (!Debugger.IsAttached)
-            //{
-            //    var options = new RewriteOptions()
-            //        .AddRedirectToHttps();
+            if (!Debugger.IsAttached)
+            {
+                var options = new RewriteOptions()
+                    .AddRedirectToHttps();
 
-            //    app.UseRewriter(options);
-            //}
+                app.UseRewriter(options);
+            }
 
             app.UseMvc(routes =>
             {
